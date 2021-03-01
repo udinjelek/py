9eedf6fcc95f16d0edbe0e6397747e175c624c84
udin jelek <udinjelek@users.noreply.github.com>
2021-03-01 15:39:55 +0700

    work update convert list to mycom

--- clipboardCopyPaste/convertColumnToHorizontalInputMycom.py
+++ clipboardCopyPaste/convertColumnToHorizontalInputMycom.py
@@ -1,16 +1,29 @@
 import clipboard
 from playsound import playsound
 
-findString = '\r\n'
+findString1 = '\r\n'
+findString2 = '\n'
 replaceString = "|"
 pasteTextIn = clipboard.paste()
-pasteTextOut = pasteTextIn.replace(findString, replaceString)
+pasteTextOut = pasteTextIn.replace(findString1, replaceString)
+pasteTextOut = pasteTextOut.replace(findString2, replaceString)
 
 if pasteTextOut[-1:] == "|" :
     pasteTextOut = pasteTextOut[:-1]
+
+comma = ","
+
+uniqueLen = len(list(set(pasteTextOut.replace(replaceString, comma).split(comma))))
+
+
 print("In" , pasteTextIn)
-print("Total Enter :", pasteTextIn.count(findString))
-print("Out", pasteTextOut)
+print("Total Enter Mode 01:", pasteTextIn.count(findString1))
+print("Total Enter Mode 02:", pasteTextIn.count(findString2))
+print("Total Unique Data  :", uniqueLen)
+
+print("")
+print("Output :")
+print(pasteTextOut)
 
 clipboard.copy(pasteTextOut)
 playsound('C:\Windows\media\Alarm03.wav')
