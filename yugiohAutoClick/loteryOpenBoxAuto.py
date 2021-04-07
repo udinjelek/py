a4ad74298db5bb95de521410b55493fff428ed22
udin jelek <home.udinjelek@users.noreply.github.com>
2021-04-07 09:09:54 +0700

    home kirim vb

---
+++ yugiohAutoClick/loteryOpenBoxAuto.py
@@ -0,0 +1,24 @@
+import pyautogui
+import time
+from playsound import playsound
+
+from pyautogui import *
+import keyboard
+import random
+import win32api, win32con
+
+while True:
+    # do something
+    time.sleep(0.5)
+    pyautogui.click(950, 820)
+    if keyboard.is_pressed("q"):
+        print("q pressed, ending loop")
+        break
+
+    time.sleep(0.5)
+    pyautogui.click(1100, 820)
+    if keyboard.is_pressed("q"):
+        print("q pressed, ending loop")
+        break
+
+playsound('C:\Windows\media\Alarm03.wav')
